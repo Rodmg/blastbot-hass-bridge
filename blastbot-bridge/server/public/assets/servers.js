fee768a8763fd494da7fbec4885cec4ba8c114c8
Rodrigo Méndez <rodmg4@gmail.com>
2023-05-30 12:43:59 -0600

    Unignore public assets (frontend build)

---
+++ blastbot-bridge/server/public/assets/servers.js
@@ -0,0 +1,14 @@
+blastbotCloudServer = {
+  protocol: 'http', // http or https
+  url: 'localhost', // url or ip
+  port: ":8888", // ":8888"
+  brokerUrl: "10.0.0.20",
+};
+blastbotHttpConfigServer = 'http://configure.blastbot.io/';
+
+// blastbotCloudServer = {
+//   protocol: 'http', // http or https
+//   url: '192.168.0.12', // url or ip
+//   port: ':8888', // ":8888"
+// };
+// blastbotHttpConfigServer = 'http://configure.blastbot.io/';
